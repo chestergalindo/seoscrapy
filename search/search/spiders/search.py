98bc01fffce8088af3140b115323a79360054241
cesar <csgalindos@hotmail.com>
2020-09-15 00:05:19 -0500

    check seo presentation

--- search/search/spiders/search.py
+++ search/search/spiders/search.py
@@ -6,7 +6,6 @@ class Item(scrapy.Spider):
     name = "seo"
     start_urls = [
         'https://www.larepublica.co/'
-        # 'https://treble.ai/'
     ]
     custom_settings = {
         'FEED_URI': 'seo.json',
@@ -22,31 +21,76 @@ class Item(scrapy.Spider):
     def parse(self, response):
         today = datetime.date.today().strftime('%d-%m-%Y')
         titlepage = response.xpath('//title/text()').get()
-        htitle = response.xpath('//h1/text()').get()
+        sizetitlepage = len(titlepage)
+        if sizetitlepage == 0:
+            rigthdimentiontitlepage = 'untitle'
+        elif sizetitlepage <= 60 and sizetitlepage >= 50:
+            rigthdimentiontitlepage = True
+        elif sizetitlepage > 60:
+            rigthdimentiontitlepage = False
+        else:
+            rigthdimentiontitlepage = False
+        numbertitlepage = len(response.xpath('//title/text()').getall())
+
         meta_description = response.xpath(
-            '//meta[contains(@name,"description")]').get()
+            '//meta[@name="description"]/@content').get()
+        sizemetadescription = len(meta_description)
+        if sizemetadescription == 0:
+            rigthdimentionmetadescription = 'undescription'
+        elif sizemetadescription <= 155:
+            rigthdimentionmetadescription = True
+        else:
+            rigthdimentionmetadescription = False
+        numbermeta_description = len(response.xpath(
+            '//meta[@name="description"]/@content').getall())
+
         meta_keywords = response.xpath(
             '//meta[contains(@name,"keywords")]').getall()
-        meta_google = response.xpath(
-            '//meta[contains(@property,"og")]').getall()
-        meta_facebook = response.xpath(
-            '//meta[contains(@property,"fb")]').getall()
-        meta_twitter = response.xpath(
-            '//meta[contains(@name,"twitter")]').getall()
-        img = response.xpath('//img[not(@alt)]').getall()
-        imgwoalt = response.xpath('//img[contains(@alt,"")]').getall()
-        bottons = response.xpath('//button[not(@href)]/text()').getall()
+        numbermeta_keywords = len(response.xpath(
+            '//meta[contains(@name,"keywords")]').getall())
+
+        htitle = response.xpath('//h1/text()').get()
+        numberhtitle = len(response.xpath('//h1/text()').getall())
+
+        imgwithoutalt = response.xpath('//img[not(@alt)]').getall()
+        numberimgwithoutalt = len(response.xpath('//img[not(@alt)]').getall())
+
+        imgwithaltempty = response.xpath('//img[@alt=""]/@src').getall()
+        numberimgwithaltempty = len(response.xpath('//img[@alt=""]').getall())
+
+        totalimg = len(response.xpath('//img').getall())
+        percentajeemptyaltinimg = (numberimgwithaltempty/totalimg)*100
+
+        # meta_google = response.xpath(
+        #     '//meta[contains(@property,"og")]').getall()
+        # meta_facebook = response.xpath(
+        #     '//meta[contains(@property,"fb")]').getall()
+        # meta_twitter = response.xpath(
+        #     '//meta[contains(@name,"twitter")]').getall()
+        # bottons = response.xpath('//button[not(@href)]/text()').getall()
 
         yield {
             'date': today,
             'titlepage': titlepage,
-            'description': meta_description,
+            'qtytitlepage': numbertitlepage,
+            'lenthtitlepage': sizetitlepage,
+            'rigthdimensiontitlepage': rigthdimentiontitlepage,
+            'metadescription': meta_description,
+            'lenthmetadescription': sizemetadescription,
+            'rigthdimensionmetadescription': rigthdimentionmetadescription,
+            'qtymetadescription': numbermeta_description,
             'keywords': meta_keywords,
-            'google': meta_google,
-            'facebook': meta_facebook,
-            'twitter': meta_twitter,
-            'title': htitle,
-            'img_without_alt': img,
-            'img_with_alt_empty': imgwoalt,
-            'bottons_with_links': bottons
+            'qtymeta_keywords': numbermeta_keywords,
+            'h1title': htitle,
+            'qtyh1title': numberhtitle,
+            'imgwithoutalt': imgwithoutalt,
+            'qtyimgwithoutalt': numberimgwithoutalt,
+            'imgwithaltempty': imgwithaltempty,
+            'qtyimgwithaltempty': numberimgwithaltempty,
+            'totalimg': totalimg,
+            'percentajeemptyaltinimg': percentajeemptyaltinimg,
+            # 'google': meta_google,
+            # 'facebook': meta_facebook,
+            # 'twitter': meta_twitter,
+            # 'bottons_with_links': bottons
         }
